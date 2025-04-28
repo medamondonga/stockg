c824b7524a652b8814bf38aaf01dc9e665b821c6
medamondonga <medamondonga4@gmail.com>
2025-04-28 23:55:25 +0200

    Accounts models and admin site

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,3 +1,28 @@
+"""
+here i create my accounts models
+"""
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """
+    User's class
+    """
+    ROLE_CHOICES = [
+        ('owner', 'Owner'),
+        ('seller', 'Seller'),
+        ('manager', 'Manager'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='owner')
+    groups = models.ManyToManyField(
+        Group,
+        related_name='accounts_user_groups',  # Nom unique
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='accounts_user_permissions',  # Nom unique
+        blank=True
+    )
+    def __str__(self):
+        return f"{self.username}"
--- src/stockg/settings.py
+++ src/stockg/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+"""
+ Thinks i've had
+"""
+AUTH_USER_MODEL = 'accounts.User'
